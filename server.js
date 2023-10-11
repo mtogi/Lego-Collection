74c3ad19ff8193a7d57d2bea41cee207b647e4fa
Mustafa Toygar Baykal <76550003+mtogi@users.noreply.github.com>
2023-10-11 16:14:05 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+
+const legoData = require("./modules/legoSets");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public')); // set static folder
+
+const HTTP_PORT = process.env.PORT || 8080; // set port
+
+// get "/"
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+// get "/"
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+// get "/lego/sets" to return all sets
+app.get('/lego/sets', (req, res) => {
+  const theme = req.query.theme;
+
+  if (theme) {
+    legoData.getSetsByTheme(theme).then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.status(404).send(`404 - ${err}`)
+    });
+  } else {
+    legoData.getAllSets().then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.status(404).send(`404 - ${err}`)
+    });
+  }
+});
+
+// get "/lego/sets/:id" to return a set by setNum
+app.get('/lego/sets/:id', (req, res) => {
+  const setNum = req.params.id;
+
+  legoData.getSetByNum(setNum).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(404).send(`404 - ${err}`)
+  });
+});
+
+// 404 error
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+});
+
+// call initialize function in legoData module
+legoData.initialize().then(() => {
+  app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+}).catch((err) => {
+  console.log(err);
+});
